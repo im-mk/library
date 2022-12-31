409e2d7c4dda10e3860639c1b44d98f06479c86c
im-mk <45291252+im-mk@users.noreply.github.com>
2022-12-31 19:23:48 +0000

    terraform s3 backup

---
+++ tf/README.md
@@ -0,0 +1,3 @@
+# Description
+
+Create S3 bucket for Terraform state files backup
---
+++ tf/variables.tf
@@ -0,0 +1,5 @@
+variable "bucket_name" {
+  default     = "mk-infra-internal"
+  description = "Name of s3 bucket"
+  type        = string
+}
